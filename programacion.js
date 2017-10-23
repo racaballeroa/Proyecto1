1f81cee82cc98b953072384e140a595649753100
racaballeroae <racaballeroa@hotmail.com>
2017-10-23 18:06:51 -0500

    se intenta dar interactividad en los satélites, que cuando uno este abierto, los demás permanezcan cerrados.

--- programacion.js
+++ programacion.js
@@ -3,17 +3,16 @@ $(document).ready(function(){
     var elementoQueLeHiceClick = elemento.currentTarget;
     $(elementoQueLeHiceClick).fadeOut(); /*fadeOut=desvanecerse*/
     $(elementoQueLeHiceClick).siblings('.informacion').fadeIn();
-    // siblings=hermanos
-    /*fadeIn=aparecer gradualmente*/
+    // siblings=hermanos              /*fadeIn=aparecer gradualmente*/
+    
   });
 
   $('.informacion > input ').click(function(elemento){
     var elementoQueLeHiceClick = elemento.currentTarget;
     $(elementoQueLeHiceClick).parent().fadeOut();
+    if ((elementoQueLeHiceClick).parent().sibiling('img').click){
+      .hidden();
+    }
     $(elementoQueLeHiceClick).parent().siblings('img').fadeIn();
   })
-  
-  if (elementoQueLeHiceClick === true){
-    
-  }
-});
+  });
